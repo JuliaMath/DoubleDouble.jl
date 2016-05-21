823be307bef711fdf31c9ef978f6e9ba530274bd
David Sanders <dpsanders@gmail.com>
2016-05-21 03:29:53 +0100

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 
 using DoubleDouble
-using Base.Tests
+using Base.Test
+
+double(x::BigFloat) = convert(Double{Float64},x)
 
 x = sqrt(2.0)
 bx = big(x)
@@ -18,19 +20,19 @@ dy = Double(y)
 dxy = dx*dy
 bxy = bx*by
 @test sx*sy == dxy
-@test x*y == float64(dxy)
+@test x*y == Float64(dxy)
 @test dxy == double(bxy)
 
-@test x+y == float64(dx+dy)
+@test x+y == Float64(dx+dy)
 @test dx+dy == double(bx+by)
 
-@test x-y == float64(dx-dy)
+@test x-y == Float64(dx-dy)
 @test dx-dy == double(bx-by)
 
-@test x/y == float64(dx/dy)
+@test x/y == Float64(dx/dy)
 @test dx/dy == double(bx/by)
 
-@test sqrt(y) == float64(sqrt(dy))
+@test sqrt(y) == Float64(sqrt(dy))
 @test sqrt(dy) == double(sqrt(by))
 
 @test rem(dxy,1.0) == double(rem(bxy,1.0))
