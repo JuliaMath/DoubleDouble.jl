1a7e86a76af61c7f192cf7ed8ce798a6d8599a33
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-11-21 09:24:11 -0500

    implement agreed approach

--- src/DoubleDouble.jl
+++ src/DoubleDouble.jl
@@ -1,39 +1,79 @@
-__precompile__()
 
 module DoubleDouble
 
-export Double, ComputeMode, Fast, Accurate
+export Double, FastDouble
 
-abstract type ComputeMode end
-struct Fast <: ComputeMode end
-struct Accurate <: ComputeMode end
+const SysFloat = Union{Float16, Float32, Float64}
 
-const CM = ComputeMode
-const SysFloat = Union{Float64, Float32}
+# Algorithmic choice is a Trait
+abstract type Trait end
+abstract type Emphasis <: Trait end
+struct Accuracy    <: Emphasis end
+struct Performance <: Emphasis end
 
-struct Double{T<:SysFloat, M<:ComputeMode} <: AbstractFloat
+const EMPHASIS     = Accuracy    # this is the default Emphasis
+const ALT_EMPHASIS = Performance
+
+const EMPHASIS_STR     = ""        # these are used in string()
+const ALT_EMPHASIS_STR = "Fast"
+
+abstract type AbstractDouble{T} <: AbstractFloat end
+
+struct Double{T<:SysFloat, E<:Emphasis} <: AbstractDouble{T}
     hi::T
     lo::T
 end
 
-# constructors
-function Double{T}(x::Number, M::Type{<:CM}=Fast) where {T<:SysFloat}
-    Double{T, M}(convert(T, x), isinf(x) ? convert(T, Inf) : zero(T))
+function Double(::Type{E}, hi::T, lo::T) where {T<:SysFloat, E<:Emphasis}
+    s = hi + lo
+    e = (hi - s) + lo
+    return Double{T,E}(s, e)
 end
 
-function Double{Float32}(x::Float64, M::Type{<:CM}=Fast)
-    z = convert(Float32, x)
-    Double{Float32, M}(u, convert(Float32, x - z))
+function Double(::Type{E}, hi::T) where {T<:SysFloat, E<:Emphasis}
+    return Double{T,E}(hi, zero(T))
 end
 
-Double(u::T, v::T, M::Type{<:CM}=Fast) where {T<:SysFloat} = Double{T, M}(u, v)
-function Double(x::BigFloat, M::Type{<:CM}=Fast)
-    z = convert(Float64, x)
-    Double{Float64, M}(z, convert(Float64, x - z))
+function Double(::Type{E}, hi::T, lo::T) where {T<:Signed, E<:Emphasis}
+    return Double(E, float(hi), float(lo))
 end
-function Double(x::T, M::Type{<:CM}=Fast) where {T<:SysFloat}
-    Double{T, M}(x, isinf(x) ? convert(T, Inf) : zero(T))
+function Double(::Type{E}, hi::T) where {T<:Signed, E<:Emphasis}
+    return Double(E, float(hi), float(zero(T)))
 end
-Double(x::Number, M::Type{<:CM}=Fast) = Double{Float64}(x, M)
 
+function Double(::Type{E}, hi::T, lo::T) where {T<:Real, E<:Emphasis}
+    s = Float64(hi + lo)
+    e = Float64((hi - T(s)) + lo)
+    return Double{Float64,E}(s,e)
+end
+function Double(::Type{E}, hi::T) where {T<:Real, E<:Emphasis}
+    s = Float64(hi)
+    e = Float64(hi - T(s))
+    return Double{Float64,E}(s,e)
+end
+
+function Double(::Type{E}, hi::T, lo::T) where {T<:Rational, E<:Emphasis}
+    return Double(E, BigFloat(hi), BigFloat(lo))
+end
+function Double(::Type{E}, hi::T) where {T<:Rational, E<:Emphasis}
+    return Double(E, BigFloat(hi))
+end
+
+Double(x::S) where S<:SysFloat = Double(EMPHASIS, x)
+Double(x::R) where R<:Real = Double(EMPHASIS, x)
+Double(x::S, y::S) where S<:SysFloat = Double(EMPHASIS, x)
+Double(x::R, y::R) where R<:Real = Double(EMPHASIS, x, y)
+
+FastDouble(x::R) where R<:Real       = Double(Performance, x)
+FastDouble(x::R, y::R) where R<:Real = Double(Performance, x, y)
+
+
+function Base.string(x::Double{T,EMPHASIS}) where T<:SysFloat
+    return string(EMPHASIS_STR,"Double(",x.hi,", ",x.lo,")")
+end
+function Base.string(x::Double{T,ALT_EMPHASIS}) where T<:SysFloat
+    return string(ALT_EMPHASIS_STR,"Double(",x.hi,", ",x.lo,")")
+end
+function Base.show(io::IO, x::Double{T,E}) where  {T<:SysFloat, E<:Emphasis}
+    print(io, string(x))
 end
