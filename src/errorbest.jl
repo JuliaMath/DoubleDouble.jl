fefb8f0c0aa259789720d1a9369baf43e24f2220
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-11-22 00:33:22 -0500

    leading comment

---
+++ src/errorbest.jl
@@ -0,0 +1,7 @@
+#=
+    All functions present in this file should be almost errorfree tranformations.
+    They should provide results that are within 2ulp of exact.
+    
+    Actual errorfree transformations are collected in "errorfree.jl".
+    They should provide results that are either exact or correctly rounded.
+=#
