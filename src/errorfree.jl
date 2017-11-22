93ce078f8b60f62e93aa57171aa77d5de2f3f0f5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-11-22 00:21:01 -0500

    three_sum, three_diff and changed names from quick to sorted

--- src/errorfree.jl
+++ src/errorfree.jl
@@ -1,9 +1,11 @@
 """
-    quick_two_sum(a, b)
+    two_sum_sorted(a, b)
 
-Computes `s = fl(a+b)` and `e = err(a+b)`. Assumes `|a| ≥ |b|`.
+*unchecked* requirement `|a| ≥ |b|`
+
+Computes `s = fl(a+b)` and `e = err(a+b)`.
 """
-@inline function quick_two_sum(a::T, b::T) where T<:SysFloat
+@inline function two_sum_sorted(a::T, b::T) where T<:SysFloat
     s = a + b
     e = b - (s - a)
     return s, e
@@ -23,11 +25,13 @@ end
 
 
 """
-    quick_two_diff(a, b)
+    two_diff_sorted(a, b)
     
-Computes `s = fl(a-b)` and `e = err(a-b)`.  Assumes `|a| ≥ |b|`.
+*unchecked* requirement `|a| ≥ |b|`
+
+Computes `s = fl(a-b)` and `e = err(a-b)`.
 """
-@inline function quick_two_diff(a::T, b::T) where T<:SysFloat
+@inline function two_diff_sorted(a::T, b::T) where T<:SysFloat
     s = a - b
     e = (a - s) - b
     s, e
@@ -66,4 +70,62 @@ Computes `s = fl(a*a)` and `e = err(a*a)`.
     p = a * a
     e = fma(a, a, -p)
     p, e
+end
+
+
+
+"""
+    three_sum_sorted(a, b, c)
+    
+*unchecked* requirement `|a| ≥ |b| ≥ |c|`
+
+Computes `s = fl(a+b+c)` and `e1 = err(a+b+c), e2 = err(e1)`.
+"""
+function three_sum_sorted(a::T,b::T,c::T) where T<:SysFloat
+    s, t = two_sum_sorted(b, c)
+    x, u = two_sum_sorted(a, s)
+    y, z = two_sum_sorted(u, t)
+    x, y = two_sum_sorted(x, y)
+    return x, y, z
+end
+
+"""
+    three_sum(a, b, c)
+    
+Computes `s = fl(a+b+c)` and `e1 = err(a+b+c), e2 = err(e1)`.
+"""
+function three_sum(a::T,b::T,c::T) where T<: SysFloat
+    s, t = two_sum(b, c)
+    x, u = two_sum(a, s)
+    y, z = two_sum(u, t)
+    x, y = two_sum_sorted(x, y)
+    return x, y, z
+end
+
+"""
+    three_diff_sorted(a, b, c)
+    
+*unchecked* requirement `|a| ≥ |b| ≥ |c|`
+
+Computes `s = fl(a-b-c)` and `e1 = err(a-b-c), e2 = err(e1)`.
+"""
+function three_diff_sorted(a::T,b::T,c::T) where T<:SysFloat
+    s, t = two_diff_sorted(-b, c)
+    x, u = two_sum_sorted(a, s)
+    y, z = two_sum_sorted(u, t)
+    x, y = two_sum_sorted(x, y)
+    return x, y, z
+end
+
+"""
+    three_diff(a, b, c)
+    
+Computes `s = fl(a-b-c)` and `e1 = err(a-b-c), e2 = err(e1)`.
+"""
+function three_diff(a::T,b::T,c::T) where T<: SysFloat
+    s, t = two_diff(-b, c)
+    x, u = two_sum(a, s)
+    y, z = two_sum(u, t)
+    x, y = two_sum_sorted(x, y)
+    return x, y, z
 end
