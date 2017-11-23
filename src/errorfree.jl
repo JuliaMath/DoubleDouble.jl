2840152ee24b231fb619e64aa91e6c4fd862facc
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-11-23 00:00:32 -0500

    move text to note/

--- src/errorfree.jl
+++ src/errorfree.jl
@@ -1,19 +1,8 @@
 #=
     All functions present in this file should be errorfree tranformations.
+
     Nearly errorfree transformations (`one_inv`, `one_sqrt`, `two_divide`, ..)
-        are collected in "errorbest.jl".
-
-
-    With
-           ◌ ≅ a real numerical magnitude, constrained to be contextually appropriate
-           ⬚ ≝ the floating point value nearest ◌
-
-          fn(⬚) an algorithmic realization approximating the mathematical 𝒇﹙◌﹚
-          
-        prevfloat ⧼ fn(ᶠˡᵒᵃᵗ) ⧼ prevfloat ⧼=   𝒇(ʳᵉᵃˡ)  =⧽ nextfloat ⧽ fn(ᶠˡᵒᵃᵗ) ⧽ nextfloat
-
-        These "errorbest" functions should be at least as accurate as shown above,
-        with an absolute or relative error within ±2ulp of the exact result 𝒇﹙⬚﹚.
+      are collected in "errorbest.jl".
 =#
 
 
