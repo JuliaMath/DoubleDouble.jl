79a188af30fb78669adc2ff3ed99233a6b65dee2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-11-27 23:41:28 -0500

    Update addsub.jl

--- test/addsub.jl
+++ test/addsub.jl
@@ -1,1 +1,9 @@
+pi2   = Double(Float64(pi)^2)
+invpi = Double(inv(Float64(pi)))
+invpi2 = invpi * invpi
 
+pi2_plus_invpi2 = pi2 + invpi2
+pi2_minus_invpi2 = pi2 - invpi2
+
+@test (pi2_plus_invpi + invpi2) - invpi == pi2_plus_invpi2
+@test (pi2_minus_invpi2 + invpi) - invpi == pi2_minus_invpi2
