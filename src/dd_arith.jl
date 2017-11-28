c0226db1041607c0c607118c097d0cce31661ed3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-11-27 20:21:50 -0500

    fill in (-)

--- src/dd_arith.jl
+++ src/dd_arith.jl
@@ -1,14 +1,14 @@
 import Base: signbit, sign, abs, (+) # (-), (*), (/), square, inv, div, rem, cld, fld, mod, divrem, fldmod, sqrt,
 
-@inline signbit(a::Double{T,E}) where {T<:SysFloat,E<:Emphasis} = signbit(a.hi)
-@inline sign(a::Double{T,E}) where {T<:SysFloat,E<:Emphasis} = sign(a.hi)
-@inline abs(a::Double{T,E}) where {T<:SysFloat,E<:Emphasis} = signbit(a) ? Double(E, -a.hi, -a.lo) : a
+@inline signbit(a::Double{T,E}) where {T<:SysFloat, E<:Emphasis} = signbit(a.hi)
+@inline sign(a::Double{T,E}) where {T<:SysFloat, E<:Emphasis} = sign(a.hi)
+@inline abs(a::Double{T,E}) where {T<:SysFloat, E<:Emphasis} = signbit(a) ? Double(E, -a.hi, -a.lo) : a
 
-@inline function (-)(a::Double{T,E}) where {T<:SysFloat,E<:Emphasis}
+@inline function (-)(a::Double{T,E}) where {T<:SysFloat, E<:Emphasis}
     return Double(-a.hi, -a.lo)
 end
 
-function (+)(a::Double{T,E}, b::T) where {T<:SysFloat,E<:Emphasis}
+function (+)(a::Double{T,E}, b::T) where {T<:SysFloat, E<:Emphasis}
     hi, lo = two_sum(a.hi, b)
     lo += a.lo
     hi, lo = two_sum_hilo(hi, lo)
@@ -16,8 +16,7 @@ function (+)(a::Double{T,E}, b::T) where {T<:SysFloat,E<:Emphasis}
     return Double(E, hi, lo)
 end
 
-
-function (+)(::Type{E}, ahi::T, alo::T, b::T) where  {T<:SysFloat,E<:Emphasis}
+function (+)(::Type{E}, ahi::T, alo::T, b::T) where  {T<:SysFloat, E<:Emphasis}
     hi, lo = two_sum(ahi, b)
     lo += alo
     hi, lo = two_sum_hilo(hi, lo)
@@ -26,7 +25,28 @@ function (+)(::Type{E}, ahi::T, alo::T, b::T) where  {T<:SysFloat,E<:Emphasis}
 end
 @inline (+)(a::T, b::Double{T,E}) where {T<:SysFloat, E<:Emphasis} = (+)(E, b.hi, b.lo, a)
 
-function (+)(::Type{E}, ahi::T, alo::T, bhi::T, blo::T) where {T<:SysFloat,E<:Emphasis}
+function (+)(::Type{E}, ahi::T, alo::T, bhi::T, blo::T) where {T<:SysFloat, E<:Emphasis}
+    hihi, hilo = two_sum(ahi, bhi)
+    hi, lo = two_sum(alo, blo)
+    hilo += hi
+function (+)(a::Double{T,E}, b::T) where {T<:SysFloat, E<:Emphasis}
+    hi, lo = two_sum(a.hi, b)
+    lo += a.lo
+    hi, lo = two_sum_hilo(hi, lo)
+
+    return Double(E, hi, lo)
+end
+
+function (+)(::Type{E}, ahi::T, alo::T, b::T) where  {T<:SysFloat, E<:Emphasis}
+    hi, lo = two_sum(ahi, b)
+    lo += alo
+    hi, lo = two_sum_hilo(hi, lo)
+
+    return Double(E, hi, lo)
+end
+@inline (+)(a::T, b::Double{T,E}) where {T<:SysFloat, E<:Emphasis} = (+)(E, b.hi, b.lo, a)
+
+function (+)(::Type{E}, ahi::T, alo::T, bhi::T, blo::T) where {T<:SysFloat, E<:Emphasis}
     hihi, hilo = two_sum(ahi, bhi)
     hi, lo = two_sum(alo, blo)
     hilo += hi
@@ -37,10 +57,9 @@ function (+)(::Type{E}, ahi::T, alo::T, bhi::T, blo::T) where {T<:SysFloat,E<:Em
 
     return Double(E, hi, lo)
 end
-@inline (+)(a::Double{T,E}, b::Double{T,E}) where {T<:SysFloat,E<:Emphasis} = (+)(E, a.hi, a.lo, b.hi, b.lo)
+@inline (+)(a::Double{T,E}, b::Double{T,E}) where {T<:SysFloat, E<:Emphasis} = (+)(E, a.hi, a.lo, b.hi, b.lo)
 
-
-function (+)(a::Double{T1,E}, b::T2) where {T1<:SysFloat,T2<:SysFloat,E<:Emphasis}
+function (+)(a::Double{T1,E}, b::T2) where {T1<:SysFloat, T2<:SysFloat, E<:Emphasis}
     if sizeof(T2) > sizeof(T1)
        Double(E, T2(a.hi), T2(a.lo)) + b
     else
@@ -48,7 +67,7 @@ function (+)(a::Double{T1,E}, b::T2) where {T1<:SysFloat,T2<:SysFloat,E<:Emphasi
    end
 end
 
-(+)(a::T2, b::Double{T1,E}) where {T1<:SysFloat,T2<:SysFloat,E<:Emphasis} = b + a
+(+)(a::T2, b::Double{T1,E}) where {T1<:SysFloat, T2<:SysFloat, E<:Emphasis} = b + a
 @inline (+)(a::Double{Float64,E}, b::Float32) where {E<:Emphasis} = a + Float64(b)
 @inline (+)(a::Float32, b::Double{Float64,E}) where {E<:Emphasis} = b + Float64(a)
 
@@ -59,7 +78,8 @@ end
 
 
 
-function (-)(a::Double{T,E}, b::T) where {T<:SysFloat,E<:Emphasis}
+
+function (-)(a::Double{T,E}, b::T) where {T<:SysFloat, E<:Emphasis}
     hi, lo = two_diff(a.hi, b)
     lo += a.lo
     hi, lo = two_sum_hilo(hi, lo)
@@ -67,31 +87,45 @@ function (-)(a::Double{T,E}, b::T) where {T<:SysFloat,E<:Emphasis}
     return Double(E, hi, lo)
 end
 
-function (-)(a::T, b::Double{T,E}) where {T<:SysFloat,E<:Emphasis}
-    hi, lo = two_diff(a, b.hi)
-    lo -= b.lo
+function (-)(::Type{E}, ahi::T, alo::T, b::T) where  {T<:SysFloat, E<:Emphasis}
+    hi, lo = two_diff(ahi, b)
+    lo += alo
     hi, lo = two_sum_hilo(hi, lo)
 
     return Double(E, hi, lo)
 end
+@inline (-)(a::T, b::Double{T,E}) where {T<:SysFloat, E<:Emphasis} = (-)(E, b.hi, b.lo, a)
 
-function (-)(a::Double{T,E}, b::Double{T,E}) where {T<:SysFloat,E<:Emphasis}
-    hihi, hilo = two_diff(a.hi, b.hi)
-    hi, lo = two_diff(a.lo, b.lo)
+function (-)(::Type{E}, ahi::T, alo::T, bhi::T, blo::T) where {T<:SysFloat, E<:Emphasis}
+    hihi, hilo = two_diff(ahi, bhi)
+    hi, lo = two_diff(alo, blo)
     hilo += hi
     hi = hihi + hilo
     hilo -= hi - hihi
     lo += hilo
     hi,lo = two_sum(hi, lo)
+
     return Double(E, hi, lo)
 end
+@inline (-)(a::Double{T,E}, b::Double{T,E}) where {T<:SysFloat, E<:Emphasis} = (-)(E, a.hi, a.lo, b.hi, b.lo)
 
+function (-)(a::Double{T1,E}, b::T2) where {T1<:SysFloat, T2<:SysFloat, E<:Emphasis}
+    if sizeof(T2) > sizeof(T1)
+       Double(E, T2(a.hi), T2(a.lo)) - b
+    else
+       a - T1(b)
+   end
+end
+
+(-)(a::T2, b::Double{T1,E}) where {T1<:SysFloat, T2<:SysFloat, E<:Emphasis} = b - a
 @inline (-)(a::Double{Float64,E}, b::Float32) where {E<:Emphasis} = a - Float64(b)
-@inline (-)(a::Float32, b::Double{Float64,E}) where {E<:Emphasis} = b - Float64(a)
-(-)(a::Double{T,E}, b::S) where {S<:Signed,T<:SysFloat,E<:Emphasis}  = a - promote_type(T,S)(b)
-(-)(a::S, b::Double{T,E}) where {S<:Signed,T<:SysFloat,E<:Emphasis}  = b - promote_type(T,S)(a)
-(-)(a::Double{T,E}, b::R) where {R<:SysReal,T<:SysFloat,E<:Emphasis} = a - Double(E, b)
-(-)(a::R, b::Double{T,E}) where {R<:SysReal,T<:SysFloat,E<:Emphasis} = b - Double(E, a)
+@inline (-)(a::Float32, b::Double{Float64,E}) where {E<:Emphasis} = Float64(a) - b
+
+(-)(a::Double{T,E}, b::S) where {S<:Signed,T<:SysFloat, E<:Emphasis}  = a - promote_type(T,S)(b)
+(-)(a::S, b::Double{T,E}) where {S<:Signed,T<:SysFloat, E<:Emphasis}  = promote_type(T,S)(a) - b
+(-)(a::Double{T,E}, b::R) where {R<:SysReal,T<:SysFloat, E<:Emphasis} = a - Double(E, b)
+(-)(a::R, b::Double{T,E}) where {R<:SysReal,T<:SysFloat, E<:Emphasis} = Double(E, a) - b
+
 
 
 
