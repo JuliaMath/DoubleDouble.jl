c01c1ed122b81c3a8f2f0ab5af335641690cef4e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-11-28 05:48:01 -0500

    Update float_arith.jl

--- src/float_arith.jl
+++ src/float_arith.jl
@@ -1,19 +1,4 @@
-function Base.:(+)(::Type{E}, a::T, b::T) where {T<:SysFloat, E<:Emphasis}
-   hi, lo = two_sum(a, b)
-   return Double{T,E}(hi, lo)
-end
-
-function Base.:(-)(::Type{E}, a::T, b::T) where {T<:SysFloat, E<:Emphasis}
-   hi, lo = two_diff(a, b)
-   return Double{T,E}(hi, lo)
-end
-
-function Base.:(*)(::Type{E}, a::T, b::T) where {T<:SysFloat, E<:Emphasis}
-   hi, lo = two_prod(a, b)
-   return Double{T,E}(hi, lo)
-end
-
-function (square)(::Type{E}, a::T) where {T<:SysFloat, E<:Emphasis}
+function square(::Type{E}, a::T) where {T<:SysFloat, E<:Emphasis}
    hi, lo = one_square(a)
    return Double{T,E}(hi, lo)
 end
@@ -23,13 +8,32 @@ function Base.inv(::Type{E}, a::T) where {T<:SysFloat, E<:Emphasis}
    return Double{T,E}(hi, lo)
 end
 
-function Base.:(/)(::Type{E}, a::T, b::T) where {T<:SysFloat, E<:Emphasis}
-   hi, lo = two_div(a, b)
-   return Double{T,E}(hi, lo)
-end
-
 function Base.sqrt(::Type{E}, a::T) where {T<:SysFloat, E<:Emphasis}
    hi, lo = one_sqrt(a)
    return Double{T,E}(hi, lo)
 end
 
+
+function Base.:(+)(::Type{E}, a::T, b::T) where {T<:SysFloat, E<:Emphasis}
+   hi, lo = two_sum(a, b)
+   return Double{T,E}(hi, lo)
+end
+@inline (+)(::Type{E}, a::F1, b::F2) where {E<:Emphasis, F1<:SysFloat, F2<:SysFloat} = (+)(E, promote(a, b)...)
+
+function Base.:(-)(::Type{E}, a::T, b::T) where {T<:SysFloat, E<:Emphasis}
+   hi, lo = two_diff(a, b)
+   return Double{T,E}(hi, lo)
+end
+@inline (-)(::Type{E}, a::F1, b::F2) where {E<:Emphasis, F1<:SysFloat, F2<:SysFloat} = (-)(E, promote(a, b)...)
+
+function Base.:(*)(::Type{E}, a::T, b::T) where {T<:SysFloat, E<:Emphasis}
+   hi, lo = two_prod(a, b)
+   return Double{T,E}(hi, lo)
+end
+@inline (*)(::Type{E}, a::F1, b::F2) where {E<:Emphasis, F1<:SysFloat, F2<:SysFloat} = (*)(E, promote(a, b)...)
+
+function Base.:(/)(::Type{E}, a::T, b::T) where {T<:SysFloat, E<:Emphasis}
+   hi, lo = two_div(a, b)
+   return Double{T,E}(hi, lo)
+end
+@inline (/)(::Type{E}, a::F1, b::F2) where {E<:Emphasis, F1<:SysFloat, F2<:SysFloat} = (/)(E, promote(a, b)...)
