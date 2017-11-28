50b02a425bb8c1a57ffb699d415691a509f749b4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-11-28 06:02:14 -0500

    some renaming

--- src/dd_arith.jl
+++ src/dd_arith.jl
@@ -40,7 +40,7 @@ function (+)(x::Double{T, E}, y::Double{T,E}) where {T<:SysFloat, E<:Emphasis}
     return Double{T,E}(hi, lo)
 end
 
-function add_hilofl(ahi::T, alo::T, b::T) where T<:SysFloat
+function add_dd_fl(ahi::T, alo::T, b::T) where T<:SysFloat
     hi, lo = two_sum(ahi, b)
     lo += alo
     hi, lo = two_sum_hilo(hi, lo)
@@ -48,7 +48,7 @@ function add_hilofl(ahi::T, alo::T, b::T) where T<:SysFloat
 end
 
 # Algorithm 6 from Tight and rigourous error bounds for basic building blocks of double-word arithmetic
-function add_hilohilo(xhi::T, xlo::T, yhi::T, ylo::T) where T<:SysFloat
+function add__dd_dd(xhi::T, xlo::T, yhi::T, ylo::T) where T<:SysFloat
     hi, lo = two_sum(xhi, yhi)
     thi, tlo = two_sum(xlo, ylo)
     c = lo + thi
@@ -103,7 +103,7 @@ function (-)(x::Double{T, E}, y::Double{T,E}) where {T<:SysFloat, E<:Emphasis}
     return Double{T,E}(hi, lo)
 end
                     
-function sub_hilofl(ahi::T, alo::T, b::T) where T<:SysFloat
+function sub_dd_fl(ahi::T, alo::T, b::T) where T<:SysFloat
     hi, lo = two_diff(ahi, b)
     lo += alo
     hi, lo = two_sum_hilo(hi, lo)
@@ -112,7 +112,7 @@ end
 
 # Algorithm 6 from Tight and rigourous error bounds for basic building blocks of double-word arithmetic
 # reworked for subraction
-function sub_hilohilo(xhi::T, xlo::T, yhi::T, ylo::T) where T<:SysFloat
+function sub__dd_dd(xhi::T, xlo::T, yhi::T, ylo::T) where T<:SysFloat
     hi, lo = two_diff(xhi, yhi)
     thi, tlo = two_diff(xlo, ylo)
     c = lo + thi
@@ -132,7 +132,7 @@ function (*)(a::Double{T,E}, b::T) where {T<:SysFloat,E<:Emphasis}
 end
 @inline (*)(a::T, b::Double{T,E}) where {T<:SysFloat,E<:Emphasis} = b * a
 
-function prod_hilofl(ahi, alo, b)
+function prod_dd_fl(ahi, alo, b)
     hi, lo = two_prod(ahi, b)
     lo += alo * b
     hi, lo = two_sum_hilo(hi, lo)
@@ -145,7 +145,7 @@ experimental relerr ldexp(3.936,-106) == ldexp(1.968, -107)
 =#
 
 # Algorithm 12 from Tight and rigourous error bounds for basic building blocks of double-word arithmetic
-function prod_hilohilo(xhi::T, xlo::T, yhi::T, ylo::T) where T<:SysFloat
+function prod__dd_dd(xhi::T, xlo::T, yhi::T, ylo::T) where T<:SysFloat
     hi, lo = two_prod(xhi, yhi)
     t = xlo * ylo
     t = fma(xhi, ylo, t)
@@ -187,7 +187,7 @@ end
 
 function (/)(::T, b::Double{T,Performance}) where {T<:SysFloat}
     hi1 = a / b.hi
-    hi, lo = prod_hilofl(b.hi, b.lo, hi1)
+    hi, lo = prod_dd_fl(b.hi, b.lo, hi1)
     xhi, xlo = two_sum(a, -hi)
     xlo -= lo
     hi2 = (xhi + xlo) / b.hi
@@ -197,7 +197,7 @@ end
 
 function (/)(a::Double{T,Performance}, b::T) where {T<:SysFloat}
     hi1 = a.hi / b
-    hi, lo = prod_hilofl(a.hi, a.lo, hi1)
+    hi, lo = prod_dd_fl(a.hi, a.lo, hi1)
     xhi, xlo = two_sum(b, -hi)
     xlo -= lo
     hi2 = (xhi + xlo) / a.hi
@@ -207,7 +207,7 @@ end
 
 function (/)(a::Double{T,Performance}, b::Double{T,Performance}) where {T<:SysFloat}
     hi1 = a.hi / b.hi
-    hi, lo = prod_hilofl(b.hi, b.lo, hi1)
+    hi, lo = prod_dd_fl(b.hi, b.lo, hi1)
     xhi, xlo = two_sum(a.hi, -hi)
     xlo -= lo
     xlo += a.lo
@@ -223,9 +223,9 @@ function (/)(x::Double{T,Accuracy}, y::Double{T,Accuracy}) where {T<:SysFloat}
     rhi = fma(-y.hi, hi, one(T))
     rlo = y.lo * hi
     rhi, rlo = two_sum_hilo(rhi, rlo)
-    rhi, rlo = prod_hilofl(rhi, rlo, hi)
-    rhi, rlo = add_hilofl(rhi, rlo, hi)
-    hi, lo = prod_hilohilo(x.hi, x.lo, rhi, rlo)
+    rhi, rlo = prod_dd_fl(rhi, rlo, hi)
+    rhi, rlo = add_dd_fl(rhi, rlo, hi)
+    hi, lo = prod__dd_dd(x.hi, x.lo, rhi, rlo)
     return Double(hi, lo)
 end
 
@@ -246,14 +246,14 @@ end
 
 function (/)(a::Double{T,Accuracy}, b::Double{T,Accuracy}) where {T<:SysFloat}
     q1 = a.hi / b.hi
-    th,tl = prod_hilofl(b.hi,b.lo,q1)
-    rh,rl = add_hilohilo(a.hi, a.lo, -th,-tl)
+    th,tl = prod_dd_fl(b.hi,b.lo,q1)
+    rh,rl = add__dd_dd(a.hi, a.lo, -th,-tl)
     q2 = rh / b.hi
-    th,tl = prod_hilofl(b.hi,b.lo,q2)
-    rh,rl = add_hilohilo(rh, rl, -th,-tl)
+    th,tl = prod_dd_fl(b.hi,b.lo,q2)
+    rh,rl = add__dd_dd(rh, rl, -th,-tl)
     q3 = rh / b.hi
     q1, q2 = two_sum_hilo(q1, q2)
-    rh,rl = add_hilofl(q1, q2, q3)
+    rh,rl = add_dd_fl(q1, q2, q3)
     return Double{T,Accuracy}(rh, rl)
 end
 
