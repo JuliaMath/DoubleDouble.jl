b9be78f45a8e452254675e4c99460d0128bbba99
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-11 18:10:50 -0500

    Create round.jl

---
+++ src/double/round.jl
@@ -0,0 +1,38 @@
+function round(x::Double{T,E}) where {T,E}
+    (notfinite(x) || isinteger(x)) && return x
+    result =  isinteger(hi(x)) ? Double(E, hi(x), zero(T)) : Double(E, round(hi(x)), zero(T))
+    return result
+end
+
+for M in (:RoundNearest, :RoundNearestTiesAway, :RoundNearestTiesUp, :RoundUp, :RoundDown, :RoundToZero)
+    @eval begin
+        function round(x::Double{T,E}, $M)
+            (notfinite(x) || isinteger(x)) && return x
+            result =  isinteger(hi(x)) ? Double(E, hi(x), zero(T)) : Double(E, round(hi(x), $M), zero(T))
+            return result
+        end
+   end
+end
+
+"""
+    rld(x, y)
+
+Round the result of x/y.
+""" rld
+
+for (F,G) in ((:rld, :round),)
+    @eval begin
+        function $F(x::Double{T,E}, y::T) where {T<:SysFloat, E}
+            z = x / y
+            return $G(z)
+        end
+        function $F(x::T, y::Double{T,E}) where {T<:SysFloat, E}
+            z = x / y
+            return $G(z)
+        end
+        function $F(x::Double{T,E}, y::Double{T,E}) where {T<:SysFloat, E}
+            z = x / y
+            return $G(z)
+        end
+    end
+end
