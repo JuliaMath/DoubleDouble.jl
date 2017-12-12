12866c36fa05e7bceccc3c6d9d39a074c06aaa8e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-12 08:47:15 -0500

    Update round.jl

--- src/double/round.jl
+++ src/double/round.jl
@@ -1,10 +1,33 @@
 import Base: round
-
+        
 function round(x::Double{T,E}) where {T,E}
     (notfinite(x) || isinteger(x)) && return x
     result =  isinteger(hi(x)) ? Double(E, hi(x), zero(T)) : Double(E, round(hi(x)), zero(T))
     return result
 end
+
+@inline function round(::Type{Double{T,E}}, x::Double{T,E}, ::Type{RoundNearest}) where {T<:SysFloat, E<:Emphasis}
+    return x
+end
+@inline function round(::Type{Double{T,E}}, x::Double{T,E}, ::Type{RoundUp}) where {T<:SysFloat, E<:Emphasis}
+    return Double(E, hi(x), nextfloat(lo(x)))
+end
+@inline function round(::Type{Double{T,E}}, x::Double{T,E}, ::Type{RoundDown}) where {T<:SysFloat, E<:Emphasis}
+    return Double(E, hi(x), nextfloat(lo(x)))
+end
+@inline function round(::Type{Double{T,E}}, x::Double{T,E}, ::Type{RoundToZero}) where {T<:SysFloat, E<:Emphasis}
+    return signbit(x) ? round(Double{T,E}, x, RoundUp} : round(Double{T,E}, x, RoundDown)
+end
+
+@inline function round(x::Double{T,E}, ::Type{R}) where {R<:RoundingMode, T, E}
+    return round(Double{T,E}, x, R)
+end
+
+for M in (:RoundNearest, :RoundNearestTiesAway, :RoundNearestTiesUp, :RoundUp, :RoundDown, :RoundToZero)
+    @eval begin
+        function round(x::T, $M) where T<:SysFloat = round(T, x, $M)
+    end
+end        
 
 for M in (:RoundNearest, :RoundNearestTiesAway, :RoundNearestTiesUp, :RoundUp, :RoundDown, :RoundToZero)
     @eval begin
