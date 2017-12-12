57bbd5bc94da9094d946020b921b465b1043946f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-12 09:00:46 -0500

    Update round.jl

--- src/double/round.jl
+++ src/double/round.jl
@@ -27,7 +27,7 @@ for T in (:Float64, :Float32, :Float16)
     @eval begin
         @inline round(::Type{$T}, x::$T, ::Type{RoundNearest}) = x
         @inline round(::Type{$T}, x::$T, ::Type{RoundUp}) = nextfloat(x)
-        @inline round(::Type{$T}, x::$T, ::Type{RoundDown}) = prevfloat(lo(x)))
+        @inline round(::Type{$T}, x::$T, ::Type{RoundDown}) = prevfloat(x)
         @inline round(::Type{$T}, x::$T, ::Type{RoundToZero}) = signbit(x) ? round($T, x, RoundUp) : round($T, x, RoundDown)
         @inline round(::Type{Double{$T,E}}, x::Double{$T,E}, ::Type{RoundNearest}) where {E} = x
         @inline round(::Type{Double{$T,E}}, x::Double{$T,E}, ::Type{RoundUp}) where {E} = Double(E, hi(x), nextfloat(lo(x)))
@@ -60,7 +60,7 @@ Round the result of x/y.
 """ rld
 
 for (F,G) in ((:rld, :round),)
-    @eval begin
+    @eval beginnext
         function $F(x::Double{T,E}, y::T) where {T<:SysFloat, E}
             z = x / y
             return $G(z)
