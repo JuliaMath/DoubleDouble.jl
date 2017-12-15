a37d6063fd25e06ea82595ce0ddcf97ddc64d3ff
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-15 09:59:38 -0500

    Create bigtypes.jl

---
+++ src/bigtypes.jl
@@ -0,0 +1,23 @@
+#=
+    minimum_bigfloat_precision(Double{T,E})
+    
+    This evaluates to the number of bits that BigFloats are to use (at minimum)
+        when interoperating with, or interconverting with, Double{T,E} values.
+    
+    The number of bits explicitly stored in the significand of a Double{T,E}:
+         significand_bits(Double{T,E}) = 2 * significand_bits(T)
+     The number of significand bits used as working precision for BigFloats
+         must exceed 2*significand_bits(Float64) to insure correct results.
+     For correctly rounded results from the elementary functions for all Float64 values,
+         working precision needs to be about 3.25 greater than sigificand_bits(Float64).
+     We can surmise that the proportional increase for Doubles is similar,
+         and use a slightly higher value to increase possible coverage.
+
+     bigfloat_precision(Double{Float64,E}) == nextpow2( 7 * 64 ) == 256
+=#
+
+minimum_bigfloat_precision(::Type{Double{T,E}}) where {T<:SysFloat, E<:Emphasis} = 
+    nextpow2(cld(7 * precision(T), 2))
+    
+bigfloat_precision(::Type{Double{T,E}}) where {T<:SysFloat, E<:Emphasis} =
+    2 * minimum_bigfloat_precision
