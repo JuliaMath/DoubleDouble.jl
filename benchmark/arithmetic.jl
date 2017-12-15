a2afc65bd7991ae106524758e48eb5a1c16402d8
Sascha Timme <sascha.timme@gmail.com>
2017-12-15 08:12:20 +0100

    Fix benchmark script

--- benchmark/arithmetic.jl
+++ benchmark/arithmetic.jl
@@ -1,31 +1,27 @@
 using BenchmarkTools
 using DoubleDouble
 
-x = Double(rand()) * Double(rand())
-y = convert(Double{Float64, Performance}, x)
-for T in [:Float32, :Float64]
-    @eval begin
-        x = Double(rand($T)) * Double(rand($T))
-        y = convert(Double{$T, Performance}, x)
-        #
-        println("$($T): Accuracy")
-        println("+")
-        @btime +($x, $x)
-        println("-")
-        @btime -($x, $x)
-        println("*")
-        @btime *($x, $x)
-        println("/")
-        @btime /($x, $x)
 
-        println("$($T): Performance")
-        println("+")
-        @btime +($y, $y)
-        println("-")
-        @btime -($y, $y)
-        println("*")
-        @btime *($y, $y)
-        println("/")
-        @btime /($y, $y)
-    end
+for T in [Float32, Float64]
+    x = Double(rand(T)) * Double(rand(T))
+    y = convert(Double{T, Performance}, x)
+    println("$(T): Accuracy")
+    println("+")
+    @btime +($x, $x)
+    println("-")
+    @btime -($x, $x)
+    println("*")
+    @btime *($x, $x)
+    println("/")
+    @btime /($x, $x)
+
+    println("$(T): Performance")
+    println("+")
+    @btime +($y, $y)
+    println("-")
+    @btime -($y, $y)
+    println("*")
+    @btime *($y, $y)
+    println("/")
+    @btime /($y, $y)
 end
