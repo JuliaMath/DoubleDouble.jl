3ca4b09a6209ca01ea276c738f2f16ff528a1a1f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-21 15:12:47 -0500

    type def

--- src/triple/Triple.jl
+++ src/triple/Triple.jl
@@ -1,1 +1,11 @@
+abstract type AbstractTriple{T} <: MultipartFloat{T} end
 
+struct Triple{T<:SysFloat, E<:Emphasis} <: AbstractTriple{T}
+    hi::T   # highest significance
+    md::T   # medium  significance
+    lo::T   # lowest  significance
+end
+
+@inline hi(x::Triple{T,E}) where {T<:SysFloat, E<:Emphasis} = x.hi
+@inline md(x::Triple{T,E}) where {T<:SysFloat, E<:Emphasis} = x.md
+@inline lo(x::Triple{T,E}) where {T<:SysFloat, E<:Emphasis} = x.lo
