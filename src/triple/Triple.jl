2e30398c349e94bfc199873f4d36e2dcb9c8deec
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-21 15:16:14 -0500

    add string, show

--- src/triple/Triple.jl
+++ src/triple/Triple.jl
@@ -9,3 +9,13 @@ end
 @inline hi(x::Triple{T,E}) where {T<:SysFloat, E<:Emphasis} = x.hi
 @inline md(x::Triple{T,E}) where {T<:SysFloat, E<:Emphasis} = x.md
 @inline lo(x::Triple{T,E}) where {T<:SysFloat, E<:Emphasis} = x.lo
+
+function Base.string(x::Triple{T,EMPHASIS}) where T<:SysFloat
+    return string(EMPHASIS_STR,"Triple(",x.hi,", ",x.md,", ",x.lo,")")
+end
+function Base.string(x::Triple{T,ALT_EMPHASIS}) where T<:SysFloat
+    return string(ALT_EMPHASIS_STR,"Triple(",x.hi,", ",x.md,", ",x.lo,")")
+end
+function Base.show(io::IO, x::Triple{T,E}) where  {T<:SysFloat, E<:Emphasis}
+    print(io, string(x))
+end
